5fe4f4afe20b8f1f47299bf882bb5062e4c9f170
Christian Gaetano <christian.gaetano@gmail.com>
2014-07-24 12:16:27 -0400

    Add LinkpointServer class This basically wraps the existing Linkpoint COM interface - in the future we may want to look into implementing the interface if they even have it set up that way. Otherwise, the idea is just to make the actual communication with Linkpoint prettier.

---
+++ linkpoint-wrapper/linkpoint-wrapper/LinkpointServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LPICOM_6_0Lib;
+
+namespace linkpoint_wrapper
+{
+    class LinkpointServer
+    {
+
+        private int mPort = 0;
+        private string mCertPath = String.Empty;
+        private string mHost = string.Empty;
+        LinkPointTxn txn;
+
+        public LinkpointServer(int Port, string CertificatePath, string Host)
+        {
+            mPort = Port;
+            mCertPath = CertificatePath;
+            mHost = Host;
+            txn = new LinkPointTxn();
+        }
+
+        public string SendXML(string XML)
+        {
+            return txn.send(mCertPath, mHost, mPort, XML);
+        }
+
+        public string GetVersion()
+        {
+            return txn.getVersion() as String;
+        }
+    }
+}
