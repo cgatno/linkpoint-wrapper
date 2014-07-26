1868921c89a3e58634cda8ba05f84563539fa2cd
Christian Gaetano <christian.gaetano@gmail.com>
2014-07-25 21:01:20 -0400

    Add some documentation

--- linkpoint-wrapper/linkpoint-wrapper/LinkpointServer.cs
+++ linkpoint-wrapper/linkpoint-wrapper/LinkpointServer.cs
@@ -8,14 +8,29 @@ using LPICOM_6_0Lib;
 
 namespace linkpoint_wrapper
 {
+    /// <summary>
+    /// A static class for quickly connecting and sending data to Linkpoint servers.
+    /// </summary>
     static class LinkpointServer
     {
+        /// <summary>
+        /// Sends raw XML data to the Linkpoint servers using the parameters specified and gives the raw XML response.
+        /// </summary>
+        /// <param name="CertificatePath">The local path to the security certificate provided by FirstData.</param>
+        /// <param name="Host">The Linkpoint server hostname or address.</param>
+        /// <param name="Port">The port through which to connect to Linkpoint servers.</param>
+        /// <param name="XML">The XML data to be sent to Linkpoint servers for processing.</param>
+        /// <returns>Returns the raw response XML from Linkpoint servers.</returns>
         public static string SendXML(string CertificatePath, string Host, int Port, string XML)
         {
             LinkPointTxn txn = new LinkPointTxn();
             return txn.send(CertificatePath, Host, Port, XML);
         }
 
+        /// <summary>
+        /// Attempts to obtain a string representation of the current version of the Linkpoint COM object.
+        /// </summary>
+        /// <returns>A string representation of the current version of the Linkpoint COM object. If the data cannot be converted to a string, a null value is returned.</returns>
         public static string GetVersion()
         {
             LinkPointTxn txn = new LinkPointTxn();
