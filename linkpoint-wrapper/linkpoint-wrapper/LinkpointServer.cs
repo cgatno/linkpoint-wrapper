349d5bb8a168c6c0d87d4db2d6d9c1e08dc3a71b
Christian Gaetano <christian.gaetano@gmail.com>
2014-08-09 17:16:30 -0400

    Add the LinkpointWrapperHelper class This class just stores values that can be used to initiate a connection to the Linkpoint servers and helps with processing some of the transactions. This is useful for applications that will be processing many transactions through one connection and a single Linkpoint account.

--- linkpoint-wrapper/linkpoint-wrapper/LinkpointServer.cs
+++ linkpoint-wrapper/linkpoint-wrapper/LinkpointServer.cs
@@ -28,6 +28,18 @@ namespace linkpoint_wrapper
         }
 
         /// <summary>
+        /// Sends raw XML to the Linkpoint servers using the parameters stored in the LinkpointWrapperHelper and gives the raw XML response.
+        /// </summary>
+        /// <param name="WrapperHelper">An initialized LinkpointWrapperHelper class containing your store and server properties.</param>
+        /// <param name="XML">The XML data to be sent to Linkpoint servers for processing.</param>
+        /// <returns>Returns the raw response XML from Linkpoint servers.</returns>
+        public static string SendXML(LinkpointWrapperHelper WrapperHelper, string XML)
+        {
+            LinkPointTxn txn = new LinkPointTxn();
+            return txn.send(WrapperHelper.SecurityCertificatePath, WrapperHelper.LinkpointHostname, WrapperHelper.LinkpointPort, XML);
+        }
+
+        /// <summary>
         /// Attempts to obtain a string representation of the current version of the Linkpoint COM object.
         /// </summary>
         /// <returns>A string representation of the current version of the Linkpoint COM object. If the data cannot be converted to a string, a null value is returned.</returns>
---
+++ linkpoint-wrapper/linkpoint-wrapper/LinkpointWrapperHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkpoint_wrapper
+{
+    class LinkpointWrapperHelper
+    {
+        public string StoreNumber { get; set; }
+        public int LinkpointPort { get; set; }
+        public string LinkpointHostname { get; set; }
+        public string SecurityCertificatePath { get; set; }
+
+        public LinkpointWrapperHelper(string StoreNumber, int LinkpointPort, string LinkpointHostname, string SecurityCertificatePath)
+        {
+            this.StoreNumber = StoreNumber;
+            this.LinkpointPort = LinkpointPort;
+            this.LinkpointHostname = LinkpointHostname;
+            this.SecurityCertificatePath = SecurityCertificatePath;
+        }
+    }
+}
