3bfe3e8116b08b030fc853c4c9c4e4c31e80f887
Christian Gaetano <christian.gaetano@gmail.com>
2014-08-29 18:42:43 -0400

    Add OrderOptions entity class

---
+++ linkpoint-wrapper/linkpoint-wrapper/LinkpointEntities/OrderOptions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+using linkpoint_wrapper.LinkpointConstants;
+
+namespace linkpoint_wrapper.LinkpointEntities
+{
+    class OrderOptions : ILinkpointEntity
+    {
+        private const string ORDER_TYPE_TAG = "ordertype";
+        public OrderType OrderType { get; set; }
+
+        private const string ORDER_MODE_TAG = "result";
+        public OrderMode OrderMode { get; set; }
+
+        private const string ORDER_OPTIONS_TAG = "orderoptions";
+
+        public string EntityXML()
+        {
+            StringBuilder outputBuilder = new StringBuilder();
+
+            using (XmlWriter writer = XmlWriter.Create(outputBuilder))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement(ORDER_OPTIONS_TAG);
+
+                writer.WriteElementString(ORDER_TYPE_TAG, OrderType.ToString());
+                writer.WriteElementString(ORDER_MODE_TAG, OrderMode.ToString());
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+
+            return outputBuilder.ToString();
+        }
+    }
+}
