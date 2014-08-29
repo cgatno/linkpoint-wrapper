7489e15c22ee958a46446615a5c90c9a4de0c1a6
Christian Gaetano <cgatno@gmail.com>
2014-08-29 14:51:02 -0400

    Add TransactionOrigin type-safe-enum

---
+++ linkpoint-wrapper/linkpoint-wrapper/LinkpointConstants/TransactionOrigin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkpoint_wrapper.LinkpointConstants
+{
+    public sealed class TransactionOrigin
+    {
+        private readonly String value;
+
+        public static readonly TransactionOrigin EmailInternet = new TransactionOrigin("ECI");
+        public static readonly TransactionOrigin MailOrderPhoneOrder = new TransactionOrigin("MOTO");
+        public static readonly TransactionOrigin PhoneOrder = new TransactionOrigin("TELEPHONE");
+        public static readonly TransactionOrigin RetailFaceToFace = new TransactionOrigin("RETAIL");
+        
+        private TransactionOrigin(String value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return value;
+        }
+    }
+}
