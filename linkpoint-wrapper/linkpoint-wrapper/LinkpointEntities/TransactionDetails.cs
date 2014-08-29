5f532e38ae265fcd7c7eb1059fb9dc27fc8c91bc
Christian Gaetano <christian.gaetano@gmail.com>
2014-08-29 18:22:15 -0400

    Remove unused (for now) references

--- linkpoint-wrapper/linkpoint-wrapper/LinkpointEntities/TransactionDetails.cs
+++ linkpoint-wrapper/linkpoint-wrapper/LinkpointEntities/TransactionDetails.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
-using System.Web;
 
 using linkpoint_wrapper.LinkpointConstants;
 
