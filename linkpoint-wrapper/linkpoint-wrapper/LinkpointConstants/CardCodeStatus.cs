2456636ab77dbcc9f50a43214da3e0b107ec18e8
Christian Gaetano <christian.gaetano@gmail.com>
2014-08-30 11:55:06 -0400

    Add CardCodeStatus type-safe-enum

---
+++ linkpoint-wrapper/linkpoint-wrapper/LinkpointConstants/CardCodeStatus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkpoint_wrapper.LinkpointConstants
+{
+    public sealed class CardCodeStatus
+    {
+        private readonly string value;
+
+        public static readonly CardCodeStatus Provided = new CardCodeStatus("provided");
+        public static readonly CardCodeStatus NotProvided = new CardCodeStatus("not_provided");
+        public static readonly CardCodeStatus Illegible = new CardCodeStatus("illegible");
+        public static readonly CardCodeStatus NotPresent = new CardCodeStatus("not_present");
+        public static readonly CardCodeStatus NoImprint = new CardCodeStatus("no_imprint");
+
+        private CardCodeStatus(string value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return value;
+        }
+    }
+}
