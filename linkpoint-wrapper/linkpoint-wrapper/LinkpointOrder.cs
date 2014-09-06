f12d9bfeee9b3a5d5de2a4b01a27f2b87225b4a5
Christian Gaetano <christian.gaetano@gmail.com>
2014-09-06 14:54:41 -0400

    Add the LinkpointOrder class This will gather all of the entities and output them in a single XML string. This can then be sent through LinkpointServer to process a transaction.

---
+++ linkpoint-wrapper/linkpoint-wrapper/LinkpointOrder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using linkpoint_wrapper.LinkpointEntities;
+
+namespace linkpoint_wrapper
+{
+    class LinkpointOrder
+    {
+        private const string ORDER_TAG = "order";
+
+        public ICollection<ILinkpointEntity> LinkpointEntities { get; set; }
+
+        public string OrderXML()
+        {
+            StringBuilder outputBuilder = new StringBuilder();
+            // we're using simple XML syntax, so we don't need to waste the resources of creating a XML writer
+            outputBuilder.Append("<" + ORDER_TAG + ">");
+            foreach (ILinkpointEntity entity in LinkpointEntities)
+            {
+                outputBuilder.Append(entity.EntityXML());
+            }
+            outputBuilder.Append("</" + ORDER_TAG + ">");
+
+            return outputBuilder.ToString();
+        }
+    }
+}
