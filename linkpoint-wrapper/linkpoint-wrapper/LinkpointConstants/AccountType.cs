04bc4adfd7f287446c4183883bccf703ebfcd649
Christian Gaetano <christian.gaetano@gmail.com>
2014-09-06 13:35:10 -0400

    Add TeleCheck entity class and type-safe-enum class AccountType

---
+++ linkpoint-wrapper/linkpoint-wrapper/LinkpointConstants/AccountType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkpoint_wrapper.LinkpointConstants
+{
+    public sealed class AccountType
+    {
+        private readonly string value;
+
+        public AccountType PersonalChecking = new AccountType("PC");
+        public AccountType PersonalSavings = new AccountType("PS");
+        public AccountType BusinessChecking = new AccountType("BC");
+        public AccountType BusinessSavings = new AccountType("BS");
+
+        private AccountType(string value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return value;
+        }
+    }
+}
